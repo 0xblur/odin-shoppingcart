c511042ebb0f6b268daac31aee594e9a5a8aac1b
cypherHades <xjadezbix@gmx.com>
2024-03-31 16:43:22 -0500

    init: vite/typescriptreact project

---
+++ tests/App.test.js
@@ -0,0 +1,4 @@
+import { App } from "../src/App.tsx";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
